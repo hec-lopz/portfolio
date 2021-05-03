3b44266c8e9180ba41ba4842316de280dfca725e
Hector Lopez <hlopezcarballo@gmail.com>
2021-05-03 03:28:00 -0500

    Modified header for new design

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,11 +4,19 @@ import "../stylus/main.styl";
 let menuOpen = false;
 const $menu_btn = document.querySelector(".mobile-btn");
 const $menu_modal = document.querySelector(".mobile-nav");
-const $menu_item = document.querySelectorAll(".mobile-nav__item");
+const $menu_item = document.querySelectorAll(".header__btn");
 
 toggleMenu({
   state: menuOpen,
   button: $menu_btn,
   menu: $menu_modal,
   menu_items: $menu_item,
+});
+
+window.addEventListener("load", () => {
+  $menu_modal.setAttribute("style", "transition: .25s ease-in transform");
+  console.log("loaded");
+  $menu_btn
+    .querySelector(".menu-btn__burger")
+    .setAttribute("style", "transition: all .5s ease;");
 });
--- src/scripts/toggleMenu.js
+++ src/scripts/toggleMenu.js
@@ -1,19 +1,19 @@
 function toggleMenu({ state, button, menu, menu_items }) {
   button.addEventListener("click", () => {
     if (state) {
-      button.classList.remove("menu-btn--open");
-      menu.classList.remove("mobile-nav--open");
+      button.classList.toggle("menu-btn--open");
+      menu.classList.toggle("header__nav--open");
       state = false;
     } else {
-      button.classList.add("menu-btn--open");
-      menu.classList.add("mobile-nav--open");
+      button.classList.toggle("menu-btn--open");
+      menu.classList.toggle("header__nav--open");
       state = true;
     }
   });
   Array.from(menu_items).forEach((item) => {
     item.addEventListener("click", () => {
       button.classList.remove("menu-btn--open");
-      menu.classList.remove("mobile-nav--open");
+      menu.classList.remove("header__nav--open");
       state = false;
     });
   });
