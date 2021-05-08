110cc3a264dde003552cc5ec4cb9467fcda20345
Hector Lopez <hlopezcarballo@gmail.com>
2021-05-08 04:03:32 -0500

    set timer to 300

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -30,4 +30,4 @@ window.addEventListener("load", () => {
 });
 
 resizeViewport();
-window.addEventListener("resize", debounce(resizeViewport, 250));
+window.addEventListener("resize", debounce(resizeViewport, 300));
