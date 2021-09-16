ef6ad86337030bfbc0cf589ec098e21f75a934a3
Hector Lopez <hlopezcarballo@gmail.com>
2021-09-16 02:12:59 -0500

    Add img and data functions

--- src/js/dataFunctions.js
+++ src/js/dataFunctions.js
@@ -1,29 +1,30 @@
 import {
   collection,
-  doc,
-  setDoc,
   getDocs,
-  Timestamp,
   addDoc,
   query,
   orderBy,
 } from "firebase/firestore";
+import { storage } from "../firebase/config";
+import { ref, getDownloadURL } from "firebase/storage";
 import { db } from "../firebase/config";
 const addData = (obj) => {
   addDoc(collection(db, "projects"), obj);
-  // console.log(collection(db, "projects"));
 };
+export const getImgUrl = async (fbRef) => {
+  const rf = ref(storage, fbRef);
+  const url = await getDownloadURL(rf);
+  return url;
+};
+
 export const getData = async () => {
   const projectsQuery = query(collection(db, "projects"), orderBy("title"));
   const response = await getDocs(projectsQuery);
-  const data = await response.docs.map(async (item) => {
+  const data = await response.docs.map((item) => {
     const itemData = item.data();
     const obj = { ...itemData, id: item.id };
     return obj;
   });
-  // Promise.all(data).then((values) => {
-  //   console.log(values);
-  // });
   return data;
 };
 
