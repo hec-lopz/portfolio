9f41868f550c3ff93ee87d40da2ba34a48e7da83
Hector Lopez <hlopezcarballo@gmail.com>
2023-05-13 12:21:37 -0600

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,17 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import './styles/normalize.css'
 import './styles/globals.css'
+import { Montserrat, Raleway } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  variable: '--font-raleway',
+})
 
 export default function RootLayout({
   children,
@@ -9,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={`${montserrat.variable} ${raleway.variable}`}>
       <body>
         <Header />
         {children}
